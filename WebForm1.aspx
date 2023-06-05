f6fbdb0d0c4c0694afe709743d40da067fdae6d1
yuvrajnanavre <108231173+yuvrajnanavre@users.noreply.github.com>
2023-06-05 15:51:56 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Senwell_Crud_Operation.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#3366CC" BorderStyle="None" BorderWidth="1px" CellPadding="4" DataKeyNames="id" EmptyDataText="No records has been added." Height="161px" OnRowCancelingEdit="OnRowCancelingEdit" OnRowDeleting="OnRowDeleting" OnRowEditing="OnRowEditing" OnRowUpdating="OnRowUpdating" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" style="margin-top: 15px">
+            <Columns>
+                <asp:TemplateField HeaderText="id">
+                    <ItemTemplate>
+                        <asp:Label ID="lblid" runat="server" Text='<%# Eval("id") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Amount">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtAmount" runat="server" Text='<%# Eval("Amount") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblAmount" runat="server" Text='<%# Eval("Amount") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Date">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtDate" runat="server" Text='<%# Eval("Date") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lbldate" runat="server" Text='<%# Eval("Date") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="CreatedBy">
+                    <ItemTemplate>
+                        <asp:Label ID="lblCreatedBy" runat="server" Text='<%# Eval("CreatedBy") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="CreatedOn">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtCreatedOn" runat="server" Visible="False"></asp:TextBox>
+                        <asp:Label ID="Label1" runat="server" Text='<%# Eval("CreatedOn") %>'></asp:Label>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblCreatedOn" runat="server" Text='<%# Eval("CreatedOn") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="ModifiedBy">
+                    <ItemTemplate>
+                        <asp:Label ID="lblModifiedBy" runat="server" Text='<%# Eval("ModifiedBy") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="ModifiedOn">
+                    <ItemTemplate>
+                        <asp:Label ID="lblModifiedOn" runat="server" Text='<%# Eval("ModifiedOn") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:CommandField ButtonType="Image" CancelImageUrl="~/Images/cancel.png" EditImageUrl="~/Images/edit_display.gif" HeaderText="Update" ShowEditButton="True" UpdateImageUrl="~/Images/Update.gif">
+                <ControlStyle Height="20px" Width="20px" />
+                <FooterStyle Wrap="True" />
+                <HeaderStyle Wrap="True" />
+                <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" Wrap="True" />
+                </asp:CommandField>
+                <asp:CommandField ButtonType="Image" DeleteImageUrl="~/Images/Deletenew.jpg" HeaderText="Delete" ShowDeleteButton="True">
+                <ControlStyle Height="20px" Width="20px" />
+                <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" Wrap="False" />
+                </asp:CommandField>
+            </Columns>
+            <FooterStyle BackColor="#99CCCC" ForeColor="#003399" />
+            <HeaderStyle BackColor="#003399" Font-Bold="True" ForeColor="#CCCCFF" />
+            <PagerStyle BackColor="#99CCCC" ForeColor="#003399" HorizontalAlign="Left" />
+            <RowStyle BackColor="White" ForeColor="#003399" />
+            <SelectedRowStyle BackColor="#009999" Font-Bold="True" ForeColor="#CCFF99" />
+            <SortedAscendingCellStyle BackColor="#EDF6F6" />
+            <SortedAscendingHeaderStyle BackColor="#0D4AC4" />
+            <SortedDescendingCellStyle BackColor="#D6DFDF" />
+            <SortedDescendingHeaderStyle BackColor="#002876" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,133 @@
+﻿using System.Web.UI.HtmlControls;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Senwell_Crud_Operation
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private SqlDataAdapter SqlDA;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+
+        }
+        void BindGrid()
+        {
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_select", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDA = new SqlDataAdapter(cmd);
+                    DataSet dt = new DataSet();
+                    SqlDA.Fill(dt);
+                    cmd.Dispose();
+                    con.Close();
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+            catch
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Error!');", true);
+            }
+        }
+        protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            this.BindGrid();
+        }
+
+        protected void OnRowCancelingEdit(object sender, EventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            this.BindGrid();
+        }
+
+        protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string Amount = (row.FindControl("txtamount") as TextBox).Text;
+            string date = (row.FindControl("txtdate") as TextBox).Text.Trim();
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr)) 
+            {
+                SqlCommand cmd = new SqlCommand("sp_update1", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Action", "UPDATE");
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@Amount", Amount);
+                cmd.Parameters.AddWithValue("@Date", date);
+                cmd.Parameters.AddWithValue("@ModifiedBy", Convert.ToString(Session["UserName"]));
+                cmd.Connection = con;
+                con.Open();
+                SqlDA = new SqlDataAdapter(cmd);
+                DataSet dt = new DataSet();
+                SqlDA.Fill(dt);
+                cmd.Dispose();
+                con.Close();
+                BindGrid();
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('" + dt.Tables[0].Rows[0][0].ToString() + "');", true);
+                GridView1.EditIndex = -1;
+                BindGrid();
+            }
+        }
+
+        protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_delete"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Connection = con;
+                    con.Open();
+                    SqlDA = new SqlDataAdapter(cmd);
+                    DataSet dt = new DataSet();
+                    SqlDA.Fill(dt);
+                    cmd.Dispose();
+                    con.Close();
+                    BindGrid();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('" + dt.Tables[0].Rows[0][0].ToString() + "');", true);
+
+                }
+            }
+            this.BindGrid();
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            this.BindGrid();
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
